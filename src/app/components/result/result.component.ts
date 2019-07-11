2c6bf96a6b24c247ad56dcad5d1a4d1a11f80f4b
Pato Marques <patoxmarques@gmail.com>
2019-07-11 15:11:46 -0300

    result page - adding icons w/ font awesome

--- src/app/components/result/result.component.ts
+++ src/app/components/result/result.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../../models/user.model';
+import { UserService } from '../../services/user.service';
+
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.css']
 })
-export class ResultComponent {}
+export class ResultComponent {
+
+    public user;
+
+    constructor(private _user: UserService) {
+        this.user = _user.getUser();
+    }
+}
