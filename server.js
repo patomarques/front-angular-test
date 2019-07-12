8b89639496311c01b1dfb3b5e73d8aa200b43e2d
Pato Marques <patoxmarques@gmail.com>
2019-07-12 19:15:43 -0300

    server.js adjusted

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express'), path = require('path');
 
 const app = express();
 
-app.use(express.static('../frontend-test/dist'));
+app.use(express.static(`${__dirname}/frontend-test/dist`));
 
 app.get('/*', (req, res) => {
- res.sendFile(path.join(__dirname, '../frontend-test/dist/index.html'));
+ res.sendFile(path.join('./frontend-test/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080, () => {
