23be9213e3972d718a1b7c33e32ef229c74ccea5
Pato Marques <patoxmarques@gmail.com>
2019-07-12 09:25:26 -0300

    heroku config

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express'), path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/dummy'));
+app.use(express.static('./dist/desafio-frontend-angular'));
 
 app.get('/*', (req, res) => {
- res.sendFile(path.join(__dirname, '/dist/dummy/index.html'));
+ res.sendFile(path.join(__dirname, '/dist/desafio-frontend-angular/index.html'));
 });
 
 app.listen(process.env.PORT || 8080, () => {
