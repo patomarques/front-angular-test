63961131c7effacea64ca647846a698d011541cc
Pato Marques <patoxmarques@gmail.com>
2019-07-12 17:21:51 -0300

    error to publish site online

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express'), path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/frontend-test'));
+app.use(express.static('./frontend-test/dist'));
 
 app.get('/*', (req, res) => {
- res.sendFile(path.join(__dirname, '/dist/frontend-test/index.html'));
+ res.sendFile(path.join(__dirname, '/frontend-test/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080, () => {
